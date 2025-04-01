75fe2a5cfa6bff2bcd54eb1a0642fcb2a9939851
Robby94gtr <robertogramazio94@gmail.com>
2025-04-02 01:01:12 +0200

    [UPD] Created const with DOM's elements, and Array for the random numbers

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// creo le variabili contenenti gli elementi dal DOM
+
+const countdown = document.getElementById('countdown');
+const numRandomList = document.getElementById('numbers-list');
+const message = document.getElementById('message');
+const button = document.querySelector('button');
+const numUser = document.querySelector('input');
+
+//  Creo l'array che conterrà i numeri casuali
+const numRandom = [''];
